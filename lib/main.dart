88769917070d7c0c3a5a4da044f88c9d05fcad32
lumpsoid <mmgeeki@gmail.com>
2023-11-30 00:40:23 +0100

    rework for provider usage fixed routes push creating new screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/settings_screen.dart';
+import 'books_manager.dart';
+import 'db/books.dart';
 import 'screens/library_screen.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+/// The route configuration.
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const LibraryWidget();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'settings',
+          builder: (BuildContext context, GoRouterState state) {
+            return const SettingsScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,13 +36,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => BookManager(BooksDb()),
+      child: MaterialApp.router(
+        routerConfig: _router,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          useMaterial3: true,
+        )
+      )
     );
   }
 }
