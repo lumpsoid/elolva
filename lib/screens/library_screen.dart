5c1e6e8afa485a898cb598678730f2cdd339fb46
lumpsoid <mmgeeki@gmail.com>
2023-12-05 19:10:40 +0100

    add file picker

--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -1,17 +1,36 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import '../book.dart';
-import '../widgets/main_nav_bar.dart';
-import '../widgets/book_card.dart' show BookCard;
 import '../books_manager.dart' show BookManager;
+import '../widgets/book_card.dart' show BookCard;
+import '../widgets/main_nav_bar.dart';
 
 class LibraryWidget extends StatelessWidget {
   const LibraryWidget({super.key});
+
+  // TODO add return the file path
+  Future<String> _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['txt'],
+    );
+
+    if (result != null) {
+      print("Selected file: ${result.files.single.path!}");
+      return result.files.single.path!;
+    } else {
+      // User canceled the picker
+    }
+    return '';
+  }
 
   // Function to show the input dialog
   Future<void> _addBookDialog(BuildContext context) async {
     String bookName = '';
     String bookAuthor = '';
+    String pathToBook = '';
 
     return showDialog(
       context: context,
@@ -38,6 +57,16 @@ class LibraryWidget extends StatelessWidget {
                     bookAuthor = value;
                   },
                 ),
+                TextButton(
+                  onPressed: () {
+                    _pickFile();
+                  },
+                  // TODO add state managment for add form
+                  // rebuild this part when user picked file
+                  child: pathToBook == ''
+                      ? const Text('Pick a file')
+                      : Text(pathToBook),
+                ),
               ],
             ),
             actions: [
@@ -45,9 +74,10 @@ class LibraryWidget extends StatelessWidget {
               // that name is empty
               ElevatedButton(
                 onPressed: () async {
-                  if (bookName.isNotEmpty) {
+                  if (bookName.isNotEmpty && pathToBook.isNotEmpty) {
                     final bookManager = context.read<BookManager>();
-                    await bookManager.createBook(bookName, bookAuthor);
+                    await bookManager.createBook(
+                        bookName, bookAuthor, pathToBook);
                     Navigator.of(context).pop();
                   }
                 },
@@ -74,14 +104,12 @@ class LibraryWidget extends StatelessWidget {
         title: const Text('My books'),
       ),
       body: ListView.builder(
-        itemCount: context.select<BookManager, int>(
-          (manager) => manager.booksList.length
-        ),
-        itemBuilder: (context, index) {
-          List<Book> bookList = context.read<BookManager>().booksList;
-          return BookCard(book: bookList[index]);
-        }
-      ),
+          itemCount: context
+              .select<BookManager, int>((manager) => manager.booksList.length),
+          itemBuilder: (context, index) {
+            List<Book> bookList = context.read<BookManager>().booksList;
+            return BookCard(book: bookList[index]);
+          }),
       bottomNavigationBar: const MainNavBar(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
